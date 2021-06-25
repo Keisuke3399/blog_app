364beef66b15defe0d4f673491a8f1cd39903b7a
Keisuke3399 <white.phantom.3399@gmail.com>
2021-06-25 18:46:25 +0900

    Fix: DETAILE修正
     # 主にSELECTタグのカテゴリをforeach文でループ表示するための修正

--- blog_public/detail.php
+++ blog_public/detail.php
@@ -4,22 +4,42 @@ require_once("../blog_classes/blog.php");
 $blog = new Blog();
 $result = $blog->getById($id);
 
+# SELECTタグのカテゴリを表示するためBloc classにメソッド追加
+$categories = $blog->getCategoryAll();
+
+// print_r($result);
+// print_r($categories);
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
   <meta charset="UTF-8">
   <title>BLOG DETAIL</title>
 </head>
+
 <body>
-  <h3>ブログ詳細</h3>
+  <h3>BLOG DETAIL</h3>
   <hr>
-  <h3>タイトル：<?= $blog->h($result['title']) ?></h3>
+  <p>ID: <?= $blog->h($result['id']) ?></p>
+  <h3>TITLE：<?= $blog->h($result['title']) ?></h3>
   <p>投稿日時：<?= $blog->h($result['post_at']) ?></p>
-  <p>カテゴリ：<?= $blog->h($blog->setCategoryName($result['category_id'])) ?></p>
-  <hr>
+  <p>CATEGORY：
+    <select name="category_id">
+      <?php foreach ($categories as $category) { ?>
+        <!-- 三項演算子 -->
+        <option value="<?= $blog->h($category['id']) ?>"
+        <?= $blog->h($category["id"]) ===
+        $blog->h($result["category_id"]) ? "selected" : "" ?> hidden>
+          <?= $blog->h($category['title']) ?>
+        </option>
+      <?php } ?>
+    </select>
+    <hr>
   <p>本文：<?= $blog->h($result['content']) ?></p>
-  <p><a href="./index.php">戻る</a></p>
+  <hr>
+  <p><a href="./index.php">BACK</a></p>
 </body>
+
 </html>
