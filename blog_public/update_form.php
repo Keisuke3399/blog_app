e77197048cb2b00ab3d47bbc8b92df6f756db0ba
Keisuke3399 <white.phantom.3399@gmail.com>
2021-06-25 18:42:26 +0900

    Fix: UPDATE修正
     # 主にSELECTタグのカテゴリをforeach文でループ表示するための修正

--- blog_public/update_form.php
+++ blog_public/update_form.php
@@ -4,35 +4,52 @@ require_once("../blog_classes/blog.php");
 $blog = new Blog();
 $result = $blog->getById($id);
 
-// print_r($result);
+# SELECTタグのカテゴリを表示するためBloc classにメソッド追加
+$categories = $blog->getCategoryAll();
 
+// print_r($result);
+// print_r($categories);
 ?>
 
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-    <meta charset="UTF-8">
-    <title>BlogForm</title>
+  <meta charset="UTF-8">
+  <title>BLOG EDIT</title>
 </head>
+
 <body>
-    <h2>ブログ更新フォーム</h2>
-    <hr>
-    <form action="../blog_classes/blog_update.php" method="POST">
-        <p>ブログタイトル：</p>
-        <input type="hidden" name="id" value="<?= $blog->h($result['id']) ?>">
-        <input type="text" name="title" value="<?= $blog->h($result['title']) ?>">
-        <p>ブログ本文：</p>
-        <textarea name="content" id="content" cols="30" rows="10"><?= $blog->h($result['content']) ?></textarea>
-        <p>カテゴリ：</p>
-        <select name="category_id">
-            <option value="1" <?php if($result['category_id'] === 1) echo "selected" ?>>日常</option>
-            <option value="2" <?php if($result['category_id'] === 2) echo "selected" ?>>プログラミング</option>
-        </select>
-				<p>
-        <input type="radio" name="publish_status" value="1" <?php if($result['publish_status'] === 1) echo "checked" ?>>公開
-        <input type="radio" name="publish_status" value="2" <?php if($result['publish_status'] === 2) echo "checked" ?>>非公開
-				</p>
-        <p><input type="submit" value="送信"></p>
-    </form>
-    <p><a href="index.php">戻る</a></p>
+  <h2>BLOG EDIT</h2>
+  <hr>
+  <form action="../blog_classes/blog_update.php" method="POST">
+    BLOG ID：
+    <input type="hidden" name="id" value="<?= $blog->h($result['id']) ?>">
+    <?= $blog->h($result['id']) ?>
+    <p>BLOG TITLE：</p>
+    <input type="text" name="title" value="<?= $blog->h($result['title']) ?>">
+    <p>BLOG CONTENT：</p>
+    <textarea name="content" id="content" cols="30" rows="10">
+    <?= $blog->h($result['content']) ?></textarea>
+    <p>CATEGORY：</p>
+    <select name="category_id">
+      <?php foreach ($categories as $category) { ?>
+        <!-- 三項演算子 -->
+        <option value="<?= $blog->h($category['id']) ?>" 
+          <?= $blog->h($category["id"]) === $blog->h($result["category_id"]) ? "selected" : "" ?>>
+          <?= $blog->h($category['title']) ?>
+        </option>
+      <?php } ?>
+    </select>
+    <p>
+      <input type="radio" name="publish_status" value="1"
+          <?php if ($result['publish_status'] === 1) echo "checked" ?>>Release
+      <input type="radio" name="publish_status" value="2"
+          <?php if ($result['publish_status'] === 2) echo "checked" ?>>Private
+    </p>
+    <p><input type="submit" value="SAVE"></p>
+  </form>
+  <p><a href="index.php">BACK</a></p>
 </body>
+
+</html>
