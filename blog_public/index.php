95829fb6279dff5ac3bafe7dde7a9622c88b5cf5
Keisuke3399 <white.phantom.3399@gmail.com>
2021-06-25 18:44:19 +0900

    Fix: TITLEなどを日本語から英語大文字など統一
    変数名などもわかりやすく変更

--- blog_public/index.php
+++ blog_public/index.php
@@ -3,7 +3,7 @@ require_once("../blog_classes/blog.php");
 
 # 取得したデータを表示
 $blog = new Blog();
-$blog_data = $blog->getAll();
+$blog_data = $blog->getBlogAll();
 
 ?>
 
@@ -12,32 +12,36 @@ $blog_data = $blog->getAll();
 
 <head>
   <meta charset="UTF-8">
-  <title>ブログ一覧</title>
+  <title>BLOG LIST</title>
 </head>
+
 <body>
-  <h3>ブログ一覧</h3>
+  <h3>BLOG LIST</h3>
   <hr>
-	<p><a href="./form.html">新規作成</a></p>
   <table border="1">
     <tr>
+      <th>ID</th>
       <th>TITLE</th>
       <th>CATEGORY</th>
-			<th>投稿日時</th>
+      <th>DATE_AND_TIME</th>
       <th>DETAIL</th>
       <th>EDIT</th>
       <th>DELETE</th>
     </tr>
-    <?php foreach ($blog_data as $column) { ?>
+    <?php foreach ($blog_data as $data) { ?>
       <tr>
-        <td><?= $blog->h($column['title']) ?></td>
-        <td><?= $blog->h($blog->setCategoryName($column['category_id'])) ?></td>
-				<td><?= $blog->h($column['post_at']) ?></td>
-        <td><a href="detail.php?id=<?= $blog->h($column['id']) ?>">詳細</a></td>
-        <td><a href="update_form.php?id=<?= $blog->h($column['id']) ?>">編集</a></td>
-        <td><a href="../blog_classes/blog_delete.php?id=<?= $blog->h($column['id']) ?>">削除</a></td>
+        <td><?= $blog->h($data['id']) ?></td>
+        <td><?= $blog->h($data['title']) ?></td>
+        <td><?= $blog->h($data['category_title']) ?></td>
+        <td><?= $blog->h($data['post_at']) ?></td>
+        <td><a href="detail.php?id=<?= $blog->h($data['id']) ?>">DETAIL</a></td>
+        <td><a href="update_form.php?id=<?= $blog->h($data['id']) ?>">EDIT</a></td>
+        <td><a href="../blog_classes/blog_delete.php?id=<?= $blog->h($data['id']) ?>">DELETE</a></td>
       </tr>
     <?php } ?>
   </table>
+  <hr>
+  <a href="./form.php">NEW</a>
 </body>
 
 </html>
